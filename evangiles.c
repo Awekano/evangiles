6efe04f0410f72298efd397fd1e45a6b68ff6439
Awekano <enzobaylesocheleau@gmail.com>
2026-06-23 09:24:57 +0200

    Ajout du code et de l'éxécutable

---
+++ evangiles.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int compteur = 0;
+int min = 0;
+int max = 25;
+int ligne_aleatoire = 0;
+
+int main(void)
+{
+    //Génération nombre aléatoire entre min et max
+    srand(time(NULL));
+    ligne_aleatoire = rand() % (max - min + 1) + min;
+
+    char ligne[1024];
+    const char *fichier = "evangiles.txt";
+    FILE *f = fopen(fichier, "r");
+    if (f == NULL)
+    {
+        printf("Erreur lors de l'ouverture du fichier %s\n", fichier);
+        return 1;
+    }
+
+    if (f != NULL)
+    {
+        while (fgets(ligne, sizeof(ligne), f) != NULL)
+        {
+            if (compteur == ligne_aleatoire)
+            {
+                printf("%s", ligne);
+                break;
+            }
+            compteur++;
+        }
+    }
+    return 0;
+}
