de2ce07a589584e93aa2e4c17dddcdc36d4698f3
Awekano <enzobaylesocheleau@gmail.com>
2026-06-23 11:53:12 +0200

    Correction chemin de fichier

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Installation de la commande et de l'éxecutable"
-chmod +x ~/evangiles
-chmod +x ~/lire_evangile
+chmod +x evangiles
+chmod +x lire_evangile
 
 echo "Déplacement des scripts et fichiers"
 
-sudo mv ~/evangiles /usr/local/bin/evangiles
-sudo mv ~/lire_evangile /usr/local/bin/lire_evangile
-sudo mv ~/evangiles.txt /usr/local/bin/evangiles.txt
+sudo mv evangiles /usr/local/bin/evangiles
+sudo mv lire_evangile /usr/local/bin/lire_evangile
+sudo mv evangiles.txt /usr/local/bin/evangiles.txt
 
 echo "Installation terminée"
 
